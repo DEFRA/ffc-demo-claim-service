effc2a6b0d270578fa6886df28975ebfec4ad6bd
John Watson <johnwatson484@hotmail.com>
2019-06-16 11:53:51 +0100

    Try catch added to queue

--- server/services/message-service.js
+++ server/services/message-service.js
@@ -3,28 +3,32 @@ const config = require('../config')
 
 module.exports = {
   publishClaim: function (claim) {
-    const messageQueue = config.messageQueue
-    amqp.connect(messageQueue, function (err, conn) {
-      if (err) {
-        console.log(err)
-      }
-      conn.createChannel(function (err, ch) {
+    try {
+      const messageQueue = config.messageQueue
+      amqp.connect(messageQueue, function (err, conn) {
         if (err) {
           console.log(err)
         }
+        conn.createChannel(function (err, ch) {
+          if (err) {
+            console.log(err)
+          }
 
-        const data = JSON.stringify(claim)
+          const data = JSON.stringify(claim)
 
-        const calculationQueue = 'calculation'
-        ch.assertQueue(calculationQueue, { durable: false })
-        ch.sendToQueue(calculationQueue, Buffer.from(data))
-        console.log('claim queued for calculation')
+          const calculationQueue = 'calculation'
+          ch.assertQueue(calculationQueue, { durable: false })
+          ch.sendToQueue(calculationQueue, Buffer.from(data))
+          console.log('claim queued for calculation')
 
-        const scheduleQueue = 'schedule'
-        ch.assertQueue(scheduleQueue, { durable: false })
-        ch.sendToQueue(scheduleQueue, Buffer.from(data))
-        console.log('claim queued for scheduling')
+          const scheduleQueue = 'schedule'
+          ch.assertQueue(scheduleQueue, { durable: false })
+          ch.sendToQueue(scheduleQueue, Buffer.from(data))
+          console.log('claim queued for scheduling')
+        })
       })
-    })
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
