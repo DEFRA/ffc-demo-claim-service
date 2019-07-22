6825ce830f91bf0bd38c54f40eed6a360d48fdb2
Matthew Collins <matthew.collins@defra.gov.uk>
2019-07-22 08:20:48 +0100

    Change message queue library from amqplib to rhea

--- server/services/message-service.js
+++ server/services/message-service.js
@@ -1,32 +1,48 @@
-const amqp = require('amqplib/callback_api')
+const container = require('rhea')
 const config = require('../config')
 
 module.exports = {
   publishClaim: function (claim) {
     try {
-      const messageQueue = config.messageQueue
-      amqp.connect(messageQueue, function (err, conn) {
-        if (err) {
-          console.log(err)
-        }
-        conn.createChannel(function (err, ch) {
-          if (err) {
-            console.log(err)
-          }
-
-          const data = JSON.stringify(claim)
-
-          const calculationQueue = 'calculation'
-          ch.assertQueue(calculationQueue, { durable: false })
-          ch.sendToQueue(calculationQueue, Buffer.from(data))
-          console.log('claim queued for calculation')
-
-          const scheduleQueue = 'schedule'
-          ch.assertQueue(scheduleQueue, { durable: false })
-          ch.sendToQueue(scheduleQueue, Buffer.from(data))
-          console.log('claim queued for scheduling')
-        })
+      const calculationQueue = 'calculation'
+      const scheduleQueue = 'schedule'
+      const data = JSON.stringify(claim)
+      let confirmed = 0
+      let sent = 0
+      let total = 2
+      container.on('sendable', (context) => {
+        sent++
+        console.log(`Sent ${sent}`)
+        context.sender.send({ body: data })
       })
+      container.on('accepted', (context) => {
+        console.log(`Confirmed ${confirmed + 1}`)
+        console.log(context)
+        if (++confirmed === total) {
+          console.log('Closing connection')
+          context.connection.close()
+        }
+      })
+      container.on('disconnected', (context) => {
+        if (context.error) {
+          console.log('%s %j', context.error, context.error)
+        }
+        sent = confirmed
+      })
+      container.on('connection_open', (context) => {
+        context.connection.open_sender(calculationQueue)
+        context.connection.open_sender(scheduleQueue)
+      })
+      const activeMQOptions = {
+        transport: config.messageQueuePort === 5672 ? 'tcp' : 'ssl',
+        port: config.messageQueuePort,
+        reconnect_limit: 10,
+        host: config.messageQueue,
+        hostname: config.messageQueue,
+        username: config.messageQueueUser,
+        password: config.messageQueuePass
+      }
+      container.connect(activeMQOptions)
     } catch (err) {
       console.log(err)
     }
