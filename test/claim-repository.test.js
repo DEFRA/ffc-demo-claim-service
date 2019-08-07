3d10fd81087c897a26078ccf72a77e8ff7979c14
Matthew Collins <matthew.collins@defra.gov.uk>
2019-08-07 10:47:42 +0100

    Tidy up further tests after comments in PR

--- test/claim-repository.test.js
+++ test/claim-repository.test.js
@@ -3,7 +3,7 @@ let MockSequelize
 let mockDb
 
 describe('Test claim repository', () => {
-  beforeEach(async () => {
+  beforeAll(async () => {
     jest.mock('../server/models')
     jest.mock('../server/models/claim', () => {
       MockSequelize = require('sequelize-mock')
@@ -17,8 +17,11 @@ describe('Test claim repository', () => {
     })
   })
 
-  test('Claim repository loads object from database', async () => {
+  beforeEach(async () => {
     claimRepository = require('../server/repository/claim-repository')
+  })
+
+  test('Claim repository loads object from database', async () => {
     // You have to push a query result into a queue, as the mock db doesn't keep track.
     // Anything you push into the results queue is returned by the next query
     mockDb.$queueResult({
@@ -35,7 +38,6 @@ describe('Test claim repository', () => {
   })
 
   test('Claim repository creates object in database', async () => {
-    claimRepository = require('../server/repository/claim-repository')
     mockDb.$queueResult({
       claimId: 'MINE123',
       propertyType: 'business',
@@ -51,18 +53,16 @@ describe('Test claim repository', () => {
     await expect(claim.claimId).toEqual('MINE123')
     await expect(claim.propertyType).toEqual('business')
     await expect(claim.accessible).toEqual(false)
-    return expect(claim.dateOfSubsidence).toEqual(new Date('2019-01-01 12:00:00'))
+    await expect(claim.dateOfSubsidence).toEqual(new Date('2019-01-01 12:00:00'))
   })
 
   test('Claim repository handles database failure', async () => {
-    claimRepository = require('../server/repository/claim-repository')
-
     mockDb.$queueFailure(new MockSequelize.ValidationError('Test error'))
 
-    return expect(claimRepository.getById('MINE123')).rejects.toThrow()
+    await expect(claimRepository.getById('MINE123')).rejects.toThrow()
   })
 
-  afterEach(async () => {
+  afterAll(async () => {
     jest.unmock('../server/models/claim')
     jest.unmock('../server/models')
   })
