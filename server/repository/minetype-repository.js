6fdf01beb0859bfd81be8de3975bf8ab1b6e55a5
Matthew Collins <matthew.collins@defra.gov.uk>
2019-08-07 10:51:31 +0100

    Remove redundant try/catch blocks

--- server/repository/minetype-repository.js
+++ server/repository/minetype-repository.js
@@ -2,15 +2,9 @@ const db = require('../models')
 
 module.exports = {
   create: async function (claimId, mineType) {
-    try {
-      let mineTypeRecord = await db.mineTypes.upsert({
-        claimId: claimId,
-        mineType: mineType
-      })
-
-      return mineTypeRecord
-    } catch (err) {
-      throw err
-    }
+    return db.mineTypes.upsert({
+      claimId: claimId,
+      mineType: mineType
+    })
   }
 }
