927ef8ec9a73b3bdfdc75899441992f8e949cb03
Matthew Collins <matthew.collins@defra.gov.uk>
2019-08-07 10:50:53 +0100

    Remove redundant try/catch blocks

--- server/repository/claim-repository.js
+++ server/repository/claim-repository.js
@@ -2,28 +2,18 @@ const db = require('../models')
 
 module.exports = {
   getById: async function (claimId) {
-    try {
-      return await db.claims.findOne({
-        where: {
-          claimId: claimId
-        }
-      })
-    } catch (err) {
-      throw err
-    }
+    return db.claims.findOne({
+      where: {
+        claimId: claimId
+      }
+    })
   },
   create: async function (claim) {
-    try {
-      const claimRecord = await db.claims.upsert({
-        claimId: claim.claimId,
-        propertyType: claim.propertyType,
-        accessible: claim.accessible,
-        dateOfSubsidence: claim.dateOfSubsidence
-      })
-
-      return claimRecord
-    } catch (err) {
-      throw err
-    }
+    return db.claims.upsert({
+      claimId: claim.claimId,
+      propertyType: claim.propertyType,
+      accessible: claim.accessible,
+      dateOfSubsidence: claim.dateOfSubsidence
+    })
   }
 }
