6a6d04e836fbbf38e09412e2ffa9545b4a5a0941
Paul Andrews <paul.andrews@defra.gov.uk>
2020-04-20 20:53:52 +0100

    validate claim message format against schema, requiring email

--- server/services/claim-message-action.js
+++ server/services/claim-message-action.js
@@ -1,9 +1,11 @@
+const claimSchema = require('../schema/claimFromMessage')
 const claimService = require('./claim-service')
 
 async function claimMessageAction (message) {
   try {
     console.log('message received - claim ', message.Body)
     const claim = JSON.parse(message.Body)
+    await claimSchema.validateAsync(claim)
     await claimService.create(claim)
   } catch (ex) {
     console.error('unable to process message ', ex)
