885faeb03f17e04a0503ebd8a74ae22933689739
Paul Andrews <paul.andrews@defra.gov.uk>
2020-04-21 10:14:28 +0100

    Updated tests

--- test/claim-service.test.js
+++ test/claim-service.test.js
@@ -3,19 +3,19 @@ describe('Test claim service', () => {
   let claimService
   let mockClaimRepository
   let mockMinetypeRepository
-  // let mockMessageService
+  let mockPublishMessageAction
 
   beforeAll(async () => {
     jest.mock('../server/repository/claim-repository')
     jest.mock('../server/repository/minetype-repository')
-    jest.mock('../server/services/message-service')
+    jest.mock('../server/services/publish-message-action')
   })
 
   beforeEach(async () => {
     jest.resetModules()
     mockClaimRepository = require('../server/repository/claim-repository')
     mockMinetypeRepository = require('../server/repository/minetype-repository')
-    // mockMessageService = require('../server/services/message-service')
+    mockPublishMessageAction = require('../server/services/publish-message-action')
     claimService = require('../server/services/claim-service')
   })
 
@@ -61,22 +61,21 @@ describe('Test claim service', () => {
     expect(mockMinetypeRepository.create).toHaveBeenCalledTimes(2)
   })
 
-  // TODO: move to publish-message-action tests
-  // test('Claim service publishes the claim to the message broker', async () => {
-  //   const claimRecord = {
-  //     claimId: 'MINE123',
-  //     propertyType: 'business',
-  //     accessible: false,
-  //     dateOfSubsidence: new Date(),
-  //     mineType: ['gold', 'iron']
-  //   }
-  //   await claimService.create(claimRecord)
-  //   expect(mockMessageService.publishClaim).toHaveBeenCalledTimes(1)
-  // })
+  test('Claim service publishes the claim to the message broker', async () => {
+    const claimRecord = {
+      claimId: 'MINE123',
+      propertyType: 'business',
+      accessible: false,
+      dateOfSubsidence: new Date(),
+      mineType: ['gold', 'iron']
+    }
+    await claimService.create(claimRecord)
+    expect(mockPublishMessageAction.publishMessageAction).toHaveBeenCalledTimes(1)
+  })
 
   afterAll(async () => {
     jest.unmock('../server/repository/claim-repository')
     jest.unmock('../server/repository/minetype-repository')
-    jest.unmock('../server/services/message-service')
+    jest.unmock('../server/services/publish-message-action')
   })
 })
