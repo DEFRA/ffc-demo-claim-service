9cf386036fc96b441ec6fb6477857285f93b2cd6
Phil Benson <phil.benson@defra.gov.uk>
2020-06-04 16:06:20 +0100

    Add message receiver

---
+++ test/message-receiver.test.js
@@ -0,0 +1,36 @@
+const MessageReceiver = require('../server/services/messaging/message-receiver')
+const MessageSender = require('../server/services/messaging/message-sender')
+const config = require('../server/config')
+
+let messageReceiver
+let messageSender
+const address = 'test-receive'
+
+const message = {
+  content: 'hello'
+}
+
+describe('message receiver', () => {
+  afterEach(async () => {
+    await messageReceiver.closeConnection()
+    await messageSender.closeConnection()
+  })
+
+  test('message receiver can receive messages', async () => {
+    expect.assertions(1)
+    let done
+    const promise = new Promise((resolve) => {
+      done = resolve
+    })
+    const testConfig = { ...config.claimQueueConfig, address }
+    messageReceiver = new MessageReceiver('test-receiver', testConfig)
+    await messageReceiver.openConnection()
+    await messageReceiver.setupReceiver((result) => done(result.hello === message.hello))
+
+    messageSender = new MessageSender('test-sender', testConfig)
+    await messageSender.openConnection()
+    await messageSender.sendMessage(message)
+
+    return expect(promise).resolves.toEqual(true)
+  })
+})
