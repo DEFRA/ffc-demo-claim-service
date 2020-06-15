2aa0cea738c623c7b9dfacfcba416f8b1bc03e5b
Mark Harrop <markharrop@yahoo.com>
2020-06-15 13:12:55 +0100

    Ensure health check endpoint contain error handling (#97)
    * Ensure health check endpoint contain error handling
    
    https://eaflood.atlassian.net/browse/PSD-318
    
    Uncaught errors in the health check endpoint can make it difficult to
    see what is unhealthy. The end point should handle the error and provide
    details, along with the error message
    
    * remove unwanted mocks and misleadling tests
    
    * add error handling and supporting test
    
    Co-authored-by: Mark Harrop <mark.harrop@defra.gov.uk>

--- server/routes/healthy.js
+++ server/routes/healthy.js
@@ -1,14 +1,22 @@
 const databaseService = require('../services/database-service')
 
+const SERVICE_UNAVAILABLE = 503
+const OK = 200
+
 module.exports = {
   method: 'GET',
   path: '/healthy',
   options: {
     handler: async (request, h) => {
-      if (await databaseService.isConnected()) {
-        return h.response('ok').code(200)
+      try {
+        if (await databaseService.isConnected()) {
+          return h.response('ok').code(OK)
+        }
+        return h.response('database unavailable').code(SERVICE_UNAVAILABLE)
+      } catch (ex) {
+        console.error('error running healthy check', ex)
+        return h.response(`error running healthy check: ${ex.message}`).code(SERVICE_UNAVAILABLE)
       }
-      return h.response('database unavailable').code(503)
     }
   }
 }
--- test/routes/healthy.test.js
+++ test/routes/healthy.test.js
@@ -1,26 +1,10 @@
 describe('Healthy test', () => {
-  const calculationSender = {}
-  let createServer
-  let databaseService
-  let messageService
-  const scheduleSender = {}
   let server
 
-  beforeAll(async () => {
-    jest.mock('../../server/services/database-service')
-    jest.mock('../../server/services/message-service')
-
-    databaseService = require('../../server/services/database-service')
-    messageService = require('../../server/services/message-service')
-
-    messageService.getCalculationSender = jest.fn().mockReturnValue(calculationSender)
-    messageService.getScheduleSender = jest.fn().mockReturnValue(scheduleSender)
-
-    calculationSender.isConnected = jest.fn().mockReturnValue(false)
-    scheduleSender.isConnected = jest.fn().mockReturnValue(false)
-
-    createServer = require('../../server')
-  })
+  jest.mock('../../server/services/message-service')
+  jest.mock('../../server/services/database-service')
+  const databaseService = require('../../server/services/database-service')
+  const createServer = require('../../server')
 
   beforeEach(async () => {
     server = await createServer()
@@ -32,25 +16,20 @@ describe('Healthy test', () => {
       method: 'GET',
       url: '/healthy'
     }
-
-    databaseService.isConnected = jest.fn().mockReturnValue(true)
-    calculationSender.isConnected = jest.fn().mockReturnValue(true)
-    scheduleSender.isConnected = jest.fn().mockReturnValue(true)
+    databaseService.isConnected.mockReturnValue(true)
 
     const response = await server.inject(options)
 
     expect(response.statusCode).toBe(200)
   })
 
-  test('GET /healthy returns 500 if database not connected', async () => {
+  test('GET /healthy returns 503 if database not connected', async () => {
     const options = {
       method: 'GET',
       url: '/healthy'
     }
 
-    databaseService.isConnected = jest.fn().mockReturnValue(false)
-    calculationSender.isConnected = jest.fn().mockReturnValue(true)
-    scheduleSender.isConnected = jest.fn().mockReturnValue(true)
+    databaseService.isConnected.mockReturnValue(false)
 
     const response = await server.inject(options)
 
@@ -58,55 +37,27 @@ describe('Healthy test', () => {
     expect(response.payload).toBe('database unavailable')
   })
 
-  test('GET /healthy returns 503 if calculation queue not connected', async () => {
+  test('GET /healthy returns 503 and error message if database check throws an error', async () => {
     const options = {
       method: 'GET',
       url: '/healthy'
     }
 
-    databaseService.isConnected = jest.fn().mockReturnValue(true)
-    calculationSender.isConnected = jest.fn().mockReturnValue(false)
-    scheduleSender.isConnected = jest.fn().mockReturnValue(true)
-
-    const response = await server.inject(options)
-
-    expect(response.statusCode).toBe(200)
-    expect(response.payload).toBe('ok')
-  })
-
-  test('GET /healthy returns 503 if schedule queue not connected', async () => {
-    const options = {
-      method: 'GET',
-      url: '/healthy'
-    }
-
-    databaseService.isConnected = jest.fn().mockReturnValue(true)
-    calculationSender.isConnected = jest.fn().mockReturnValue(true)
-    scheduleSender.isConnected = jest.fn().mockReturnValue(false)
-
-    const response = await server.inject(options)
-
-    expect(response.statusCode).toBe(200)
-    expect(response.payload).toBe('ok')
-  })
-
-  test('GET /healthy returns 503 with appropriate message if all downstream services are disconnected', async () => {
-    const options = {
-      method: 'GET',
-      url: '/healthy'
-    }
-
-    databaseService.isConnected = jest.fn().mockReturnValue(false)
-    calculationSender.isConnected = jest.fn().mockReturnValue(false)
-    scheduleSender.isConnected = jest.fn().mockReturnValue(false)
+    const errorMessage = 'database connection timeout'
+    databaseService.isConnected.mockImplementation(() => { throw new Error(errorMessage) })
 
     const response = await server.inject(options)
 
     expect(response.statusCode).toBe(503)
-    expect(response.payload).toBe('database unavailable')
+    expect(response.payload).toBe(`error running healthy check: ${errorMessage}`)
   })
 
   afterEach(async () => {
     await server.stop()
+    jest.clearAllMocks()
+  })
+
+  afterAll(() => {
+    jest.resetAllMocks()
   })
 })
