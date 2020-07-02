db3bfd13e2021fbe18e5bb1c872edac37d2694e4
Paul Andrews <paul.andrews@defra.gov.uk>
2020-07-02 22:46:22 +0100

    sleep

--- server/models/index.js
+++ server/models/index.js
@@ -9,8 +9,10 @@ async function db () {
   const config = require(path.join(__dirname, '/../config')).database[env]
   const db = {}
 
+  await new Promise(resolve => setTimeout(resolve, 10000))
+
   let sequelize
-  if (await config.use_env_variable) {
+  if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config)
   } else {
     sequelize = new Sequelize(config.database, config.username, config.password, config)
