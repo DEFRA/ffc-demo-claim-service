09747529c7aec7b338984ebe090d2f74544e686a
Paul Andrews <paul.andrews@defra.gov.uk>
2020-07-02 19:32:49 +0100

    Test postgres creds

--- server/config/database-config.js
+++ server/config/database-config.js
@@ -1,6 +1,15 @@
+const auth = require('@azure/ms-rest-nodeauth')
+
+async function getPostgresCreds () {
+  const creds = await auth.loginWithVmMSI({ clientId: process.env.CLIENT_ID, resource: 'https://ossrdbms-aad.database.windows.net' })
+  const token = await creds.getToken()
+  console.log(`Postges token: ${token.accessToken}`)
+  return token.accessToken
+}
+
 const dbConfig = {
   username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
+  password: getPostgresCreds(),
   database: process.env.POSTGRES_DB || 'mine_claims',
   host: process.env.POSTGRES_HOST || 'localhost',
   port: process.env.POSTGRES_PORT || 5432,
