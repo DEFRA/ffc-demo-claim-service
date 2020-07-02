7a6291f0efb0ffa3607cc280f5cad5774b00eced
Paul Andrews <paul.andrews@defra.gov.uk>
2020-07-02 21:56:01 +0100

    await

--- server/models/index.js
+++ server/models/index.js
@@ -10,7 +10,7 @@ async function getDb () {
   const db = {}
 
   let sequelize
-  if (config.use_env_variable) {
+  if (await config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config)
   } else {
     sequelize = new Sequelize(config.database, config.username, config.password, config)
