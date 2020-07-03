553ae86d2cbc0b925c5dcb72043266044efe3a52
Paul Andrews <paul.andrews@defra.gov.uk>
2020-07-03 09:28:22 +0100

    Clean up 2

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const hapi = require('@hapi/hapi')
 const config = require('./config')
+const messageService = require('./services/message-service')
 
 async function createServer () {
-  await config()
-  const messageService = require('./services/message-service')
-
   // Create the hapi server
   const server = hapi.server({
     port: config.port,
@@ -28,17 +26,12 @@ async function createServer () {
 
   await messageService.registerQueues()
 
-  // Testing
-  // await messageService.publishClaim({ message: 'this is a test' })
-
   process.on('SIGTERM', async function () {
-    console.log('SIGTERM')
     await messageService.closeConnections()
     process.exit(0)
   })
 
   process.on('SIGINT', async function () {
-    console.log('SIGINT')
     await messageService.closeConnections()
     process.exit(0)
   })
