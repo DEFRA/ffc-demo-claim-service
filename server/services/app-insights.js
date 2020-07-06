0b17be224961be1e642c3e782a017e84076d74ee
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-07-06 18:41:37 +0100

    feat: log state of App Insights

--- server/services/app-insights.js
+++ server/services/app-insights.js
@@ -3,9 +3,13 @@ const appInsights = require('applicationinsights')
 function setup () {
   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
     appInsights.setup().start()
+    appInsights.defaultClient.config.maxBatchSize = 1
     const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
     const appName = process.env.APPINSIGHTS_CLOUDROLE
     appInsights.defaultClient.context.tags[cloudRoleTag] = appName
+    console.log('APPINSIGHTS_INSTRUMENTATIONKEY found. Application Insights has been turned on.')
+  } else {
+    console.log('No APPINSIGHTS_INSTRUMENTATIONKEY found. Application Insights is not active.')
   }
 }
 
