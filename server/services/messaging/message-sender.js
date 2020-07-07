c8af565faeb1d984ea82a4f05794af50fe4ab3be
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-07-07 17:12:09 +0100

    feat: add dependency tracing to message sender

--- server/services/messaging/message-sender.js
+++ server/services/messaging/message-sender.js
@@ -1,3 +1,4 @@
+const appInsights = require('applicationinsights')
 const { getSenderConfig } = require('./config-helper')
 const MessageBase = require('./message-base')
 
@@ -16,14 +17,32 @@ class MessageSender extends MessageBase {
   }
 
   async sendMessage (message) {
+    let startTime
+    let success = true
+    let resultCode = 200
+
     const data = this.decodeMessage(message)
     const sender = await this.connection.createAwaitableSender(this.senderConfig)
     try {
-      console.log(`${this.name} sending message`, data)
-      const delivery = await sender.send({ body: data })
+      startTime = Date.now()
+      const correlationContext = appInsights.getCorrelationContext()
+
+      const traceParentTraceId = correlationContext.operation.traceparent.traceId
+      const msg = { body: data, correlation_id: traceParentTraceId }
+      console.log(msg)
+
+      console.log(`${this.name} sending message`, msg)
+      const delivery = await sender.send(msg)
       console.log(`message sent ${this.name}`)
       return delivery
+    } catch (error) {
+      success = false
+      resultCode = 500
+      console.error('failed to send message', error)
+      throw error
     } finally {
+      const duration = Date.now() - startTime
+      appInsights.defaultClient.trackDependency({ data, dependencyTypeName: 'AMQP', duration, name: this.name, resultCode, success, target: this.senderConfig.target.address })
       await sender.close()
     }
   }
