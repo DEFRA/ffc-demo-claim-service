c8cae2c291f545536707f7a7a169923192b9a5d9
John Watson <johnwatson484@hotmail.com>
2020-07-10 13:07:34 +0100

    Handle pod identity or sconnection string

--- server/services/messaging/message-base.js
+++ server/services/messaging/message-base.js
@@ -3,8 +3,20 @@ const { ServiceBusClient } = require('@azure/service-bus')
 class MessageBase {
   constructor (name, config) {
     this.name = name
+    this.createClient(config)
+  }
+
+  createClient (config) {
+    this.sbClient = config.username ? this.createClientFromConnectionString(config) : this.createClientFromAADToken(config)
+  }
+
+  createClientFromConnectionString (config) {
     const connectionString = `Endpoint=sb://${config.host}/;SharedAccessKeyName=${config.username};SharedAccessKey=${config.password}`
-    this.sbClient = ServiceBusClient.createFromConnectionString(connectionString)
+    return ServiceBusClient.createFromConnectionString(connectionString)
+  }
+
+  createClientFromAADToken (config) {
+    // TODO add Azure Pod Identity logic
   }
 
   async closeConnection () {
