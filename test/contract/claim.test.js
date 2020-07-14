cd76771f2493fb56d4391be62371d0c3bfc3a8e5
John Watson <johnwatson484@hotmail.com>
2020-07-14 15:47:41 +0100

    Consumer contract test added

---
+++ test/contract/claim.test.js
@@ -0,0 +1,41 @@
+describe('Schedule message contract test', () => {
+  const path = require('path')
+  const { MessageConsumerPact } = require('@pact-foundation/pact')
+  const Matchers = require('@pact-foundation/pact/dsl/matchers')
+  const { claimMessageAction } = require('../../server/services/message-action')
+  const dbHelper = require('../db-helper')
+  const { publishClaim } = require('../../server/services/message-service')
+  let messagePact
+
+  beforeAll(async () => {
+    await dbHelper.truncate()
+
+    messagePact = new MessageConsumerPact({
+      consumer: 'ffc-demo-claim-service',
+      provider: 'ffc-demo-web',
+      log: path.resolve(process.cwd(), 'test-output', 'pact.log'),
+      dir: path.resolve(process.cwd(), 'test-output')
+    })
+  })
+
+  afterAll(() => {
+    dbHelper.close()
+  })
+
+  test('messageAction can process message', async () => {
+    await messagePact
+      .given('valid message')
+      .expectsToReceive('a request for new claim')
+      .withContent({
+        claimId: Matchers.like('MINE123'),
+        propertyType: Matchers.like('business'),
+        dateOfSubsidence: Matchers.iso8601DateTime(),
+        mineType: Matchers.like('["gold"]'),
+        email: Matchers.email()
+      })
+      .withMetadata({
+        'content-type': 'application/json'
+      })
+      .verify(message => claimMessageAction(message.contents, publishClaim))
+  })
+})
