4b8d5d81baf53647c7bdbe3b2a4c18b94d02ff6e
John Watson <johnwatson484@hotmail.com>
2020-07-14 10:07:45 +0100

    Restore connection string only message base

--- server/services/messaging/message-base.js
+++ server/services/messaging/message-base.js
@@ -3,21 +3,8 @@ const { ServiceBusClient } = require('@azure/service-bus')
 class MessageBase {
   constructor (name, config) {
     this.name = name
-    this.config = config
-    this.createClient()
-  }
-
-  createClient () {
-    this.sbClient = this.config.username ? this.createClientFromConnectionString(this.config) : this.createClientFromAADToken(this.config)
-  }
-
-  createClientFromConnectionString (config) {
     const connectionString = `Endpoint=sb://${config.host}/;SharedAccessKeyName=${config.username};SharedAccessKey=${config.password}`
-    return ServiceBusClient.createFromConnectionString(connectionString)
-  }
-
-  createClientFromAADToken (config) {
-    // TODO add Azure Pod Identity logic
+    this.sbClient = ServiceBusClient.createFromConnectionString(connectionString)
   }
 
   async closeConnection () {
