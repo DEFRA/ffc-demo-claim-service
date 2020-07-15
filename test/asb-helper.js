8c0b5eed26a7d439487fa5e6fd8cc0107b41a783
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-07-15 12:33:28 +0100

    feat: clear ASB queues for tests

---
+++ test/asb-helper.js
@@ -0,0 +1,57 @@
+const { ReceiveMode, ServiceBusClient } = require('@azure/service-bus')
+const config = require('../server/config/mq-config')
+
+// When calling this witin a test script, ensure there is a generous timeout
+// for the connections to complete within. `30000` should be good
+async function clearQueue (queueName) {
+  // There are three queues with potentially three different hosts and
+  // credentials, however, atm there is only the single instance
+  let sbClient
+  try {
+    const connectionString = `Endpoint=sb://${config.claimQueue.host}/;SharedAccessKeyName=${config.claimQueue.username};SharedAccessKey=${config.claimQueue.password}`
+    sbClient = ServiceBusClient.createFromConnectionString(connectionString)
+
+    const queueAddress = config[queueName].address
+    const queueClient = sbClient.createQueueClient(queueAddress)
+    const receiver = queueClient.createReceiver(ReceiveMode.peekLock)
+
+    console.log(`Setup to receive messages from '${queueAddress}'.`)
+    // there shouldn't be more than a handful of messages in any queue and they
+    // are recieved in blocks of `batchSize`, in as many `batches` as specified
+    const batches = 10
+    for (let j = 0; j < batches; j++) {
+      console.log(`Receiving messages, batch ${j + 1}.`)
+      const batchSize = 20
+      const messages = await receiver.receiveMessages(batchSize, 5)
+      const receivedBatchSize = messages.length
+      console.log(`Received ${receivedBatchSize} messages.`)
+
+      for (let k = 0; k < receivedBatchSize; k++) {
+        console.log(`Deleting message with index: '${k}' and id: '${messages[k].messageId}' from '${queueAddress}'.`)
+        await messages[k].complete()
+      }
+
+      if (receivedBatchSize <= batchSize) {
+        console.log(`No more messages in: '${queueAddress}'.`)
+        break
+      }
+    }
+    await queueClient.close()
+  } catch (err) {
+    console.log(err)
+    throw err
+  } finally {
+    await sbClient.close()
+  }
+}
+
+async function clearAllQueues () {
+  await clearQueue('calculationQueue')
+  await clearQueue('claimQueue')
+  await clearQueue('scheduleQueue')
+}
+
+module.exports = {
+  clearAllQueues,
+  clearQueue
+}
--- test/db-helper.js
+++ test/db-helper.js
@@ -1,4 +1,3 @@
-
 const db = require('../server/models')
 
 async function truncate () {
