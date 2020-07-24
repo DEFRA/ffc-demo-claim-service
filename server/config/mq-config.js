71d9d6b60df437ae69a5753961dc4228f3b3074a
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-07-24 10:39:15 +0100

    feat: mq creds are not needed in Helm chart

--- server/config/mq-config.js
+++ server/config/mq-config.js
@@ -22,19 +22,19 @@ const mqConfig = {
   },
   calculationQueue: {
     address: process.env.CALCULATION_QUEUE_ADDRESS,
-    username: process.env.CALCULATION_QUEUE_USER,
-    password: process.env.CALCULATION_QUEUE_PASSWORD
+    username: process.env.MESSAGE_QUEUE_USER,
+    password: process.env.MESSAGE_QUEUE_PASSWORD
   },
   scheduleQueue: {
     address: process.env.SCHEDULE_QUEUE_ADDRESS,
-    username: process.env.SCHEDULE_QUEUE_USER,
-    password: process.env.SCHEDULE_QUEUE_PASSWORD,
+    username: process.env.MESSAGE_QUEUE_USER,
+    password: process.env.MESSAGE_QUEUE_PASSWORD,
     sendTimeoutInSeconds: process.env.SEND_TIMEOUT_IN_SECONDS
   },
   claimQueue: {
     address: process.env.CLAIM_QUEUE_ADDRESS,
-    username: process.env.CLAIM_QUEUE_USER,
-    password: process.env.CLAIM_QUEUE_PASSWORD
+    username: process.env.MESSAGE_QUEUE_USER,
+    password: process.env.MESSAGE_QUEUE_PASSWORD
   }
 }
 
