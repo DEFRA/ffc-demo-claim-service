823b22ec4869d5e2bfbe01e55e29d57bb7f0732d
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-07-26 18:45:17 +0100

    fix: ensure models are awaited

--- test/db-helper.js
+++ test/db-helper.js
@@ -1,19 +1,20 @@
 const db = require('../server/models')
 
 async function truncate () {
-  await db.claims.destroy({ truncate: { casecade: true } })
-  await db.mineTypes.destroy({ truncate: true })
+  const models = await db
+  models.claims.destroy({ truncate: { casecade: true } })
+  models.mineTypes.destroy({ truncate: true })
 }
 
 async function createClaimRecords (claims) {
-  await db.claims.bulkCreate(claims)
+  (await db).claims.bulkCreate(claims)
 }
 async function createMineTypeRecords (mineTypes) {
-  await db.mineTypes.bulkCreate(mineTypes)
+  (await db).mineTypes.bulkCreate(mineTypes)
 }
 
 async function close () {
-  await db.sequelize.close()
+  (await db).sequelize.close()
 }
 
 module.exports = {
