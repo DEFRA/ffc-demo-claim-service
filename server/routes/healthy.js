a8098e4e02d3960798afec6f3cedbe245dcece7e
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-07-27 16:43:13 +0100

    fix: authenticate doesn't return truthy value

--- server/routes/healthy.js
+++ server/routes/healthy.js
@@ -9,10 +9,8 @@ module.exports = {
   options: {
     handler: async (request, h) => {
       try {
-        if (await (await sequelize).authenticate()) {
-          return h.response('ok').code(OK)
-        }
-        return h.response('database unavailable').code(SERVICE_UNAVAILABLE)
+        await (await sequelize).authenticate()
+        return h.response('ok').code(OK)
       } catch (ex) {
         console.error('error running healthy check', ex)
         return h.response(`error running healthy check: ${ex.message}`).code(SERVICE_UNAVAILABLE)
