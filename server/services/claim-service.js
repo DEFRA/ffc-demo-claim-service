06ae3fbaa357a325e232722bf2d964a83f1dddd1
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-07-27 12:33:55 +0100

    fix: remove additional logging

--- server/services/claim-service.js
+++ server/services/claim-service.js
@@ -4,7 +4,6 @@ const mineTypeRepository = require('../repository/minetype-repository')
 module.exports = {
   create: async function (claim, publisher) {
     const existingClaim = await claimRepository.getById(claim.claimId)
-    console.log('existingClaim', existingClaim)
     if (existingClaim != null) {
       console.log('Found existing claim ', existingClaim)
       return existingClaim
