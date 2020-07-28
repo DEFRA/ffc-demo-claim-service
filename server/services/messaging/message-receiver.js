6bf28a0e3a3f746218e7b38eb504201cd9109981
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-07-29 00:22:34 +0100

    feat: further simplification of message receiver

--- server/services/messaging/message-receiver.js
+++ server/services/messaging/message-receiver.js
@@ -4,26 +4,23 @@ const MessageBase = require('./message-base')
 class MessageReceiver extends MessageBase {
   constructor (name, config, credentials, action) {
     super(name, config, credentials)
-    this.receiver = this.queueClient.createReceiver(ReceiveMode.peekLock)
-    this.registerEvents = this.registerEvents.bind(this)
-    this.registerEvents(action)
+    this.receiverHandler = this.receiverHandler.bind(this)
+    this.action = action
+    const receiver = this.queueClient.createReceiver(ReceiveMode.peekLock)
+    receiver.registerMessageHandler(this.receiverHandler, this.receiverError)
   }
 
-  registerEvents (action) {
-    const receiverError = (error) => {
-      console.log(error)
-    }
+  receiverError (error) {
+    console.log(error)
+  }
 
-    const receiverHandler = async (message) => {
-      console.log(`${this.name} received message`, message.body)
-      try {
-        await action(message.body)
-      } catch (ex) {
-        console.error(`${this.name} error with message`, ex)
-      }
+  async receiverHandler (message) {
+    console.log(`${this.name} received message`, message.body)
+    try {
+      await this.action(message.body)
+    } catch (ex) {
+      console.error(`${this.name} error with message`, ex)
     }
-
-    this.receiver.registerMessageHandler(receiverHandler, receiverError)
   }
 }
 
