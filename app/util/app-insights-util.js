f76e063a05aa15d790f3f1248d1bd650ab4dc279
fareedmohammed <fareed.mohammed.uk@gmail.com>
2020-11-03 18:53:29 +0000

    fix: failing unit tests

--- app/util/app-insights-util.js
+++ app/util/app-insights-util.js
@@ -2,14 +2,21 @@ const appInsights = require('applicationinsights')
 
 module.exports = {
   setSessionId: (correlationId) => {
-    const sessionIdTag = appInsights.defaultClient.context.keys.sessionId
-    appInsights.defaultClient.context.tags[sessionIdTag] = correlationId
+    if (appInsights.defaultClient !== null && appInsights.defaultClient !== undefined) {
+      const sessionIdTag = appInsights.defaultClient.context.keys.sessionId
+      appInsights.defaultClient.context.tags[sessionIdTag] = correlationId
+    }
   },
   getSessionId: (correlationId) => {
-    const sessionIdTag = appInsights.defaultClient.context.keys.sessionId
-    return appInsights.defaultClient.context.tags[sessionIdTag]
+    if (appInsights.defaultClient !== null && appInsights.defaultClient !== undefined) {
+      const sessionIdTag = appInsights.defaultClient.context.keys.sessionId
+      return appInsights.defaultClient.context.tags[sessionIdTag]
+    }
+    return null
   },
   logTraceMessage: (message) => {
-    appInsights.defaultClient.trackTrace({ message })
+    if (appInsights.defaultClient !== null && appInsights.defaultClient !== undefined) {
+      appInsights.defaultClient.trackTrace({ message })
+    }
   }
 }
