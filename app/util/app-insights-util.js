fb3316540a2ce46d8ca592fc4f607484a132d1c7
fareedmohammed <fareed.mohammed.uk@gmail.com>
2020-11-04 11:32:03 +0000

    fix: removed null check as per PR feedback

--- app/util/app-insights-util.js
+++ app/util/app-insights-util.js
@@ -2,20 +2,20 @@ const appInsights = require('applicationinsights')
 
 module.exports = {
   setSessionId: (correlationId) => {
-    if (appInsights.defaultClient !== null && appInsights.defaultClient !== undefined) {
+    if (appInsights.defaultClient !== undefined) {
       const sessionIdTag = appInsights.defaultClient.context.keys.sessionId
       appInsights.defaultClient.context.tags[sessionIdTag] = correlationId
     }
   },
   getSessionId: (correlationId) => {
-    if (appInsights.defaultClient !== null && appInsights.defaultClient !== undefined) {
+    if (appInsights.defaultClient !== undefined) {
       const sessionIdTag = appInsights.defaultClient.context.keys.sessionId
       return appInsights.defaultClient.context.tags[sessionIdTag]
     }
     return null
   },
   logTraceMessage: (message) => {
-    if (appInsights.defaultClient !== null && appInsights.defaultClient !== undefined) {
+    if (appInsights.defaultClient !== undefined) {
       appInsights.defaultClient.trackTrace({ message })
     }
   }
