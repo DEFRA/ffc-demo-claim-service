5647db6d24189e9534c6c8fe6ad0feb471ef8418
John Watson <johnwatson484@hotmail.com>
2020-11-04 15:27:53 +0000

    App insights added

--- app/services/app-insights.js
+++ app/services/app-insights.js
@@ -2,7 +2,10 @@ const appInsights = require('applicationinsights')
 
 function setup () {
   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
-    appInsights.setup().start()
+    appInsights.setup()
+      .setAutoCollectConsole(process.env.APPINSIGHTS_CONSOLE_DEBUG_ENABLED, process.env.APPINSIGHTS_CONSOLE_DEBUG_ENABLED)
+      .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+      .start()
     console.log('App Insights Running')
     const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
     const appName = process.env.APPINSIGHTS_CLOUDROLE
