a38c488b62cd06bbeeafc0bbfb423e703caafcf4
John Watson <johnwatson484@hotmail.com>
2020-11-20 17:26:45 +0000

    Move env vars

--- app/notify/index.js
+++ app/notify/index.js
@@ -2,7 +2,7 @@ const { NotifyClient } = require('notifications-node-client')
 const config = require('../config')
 
 async function sendEmailNotification (claim) {
-  if (config.isProd) {
+  if (config.notifyApiKey) {
     const notifyClient = new NotifyClient(config.notifyApiKey)
     await notifyClient.sendEmail(config.notifyEmailTemplateKey, claim.email, {
       personalisation: { claimId: claim.claimId }
