08745ab93207c684e9886230855b4953034b9495
John Watson <johnwatson484@hotmail.com>
2020-11-20 17:20:00 +0000

    Only use notify in prod

--- app/notify/index.js
+++ app/notify/index.js
@@ -2,7 +2,7 @@ const { NotifyClient } = require('notifications-node-client')
 const config = require('../config')
 
 async function sendEmailNotification (claim) {
-  if (config.notifyApiKey.length) {
+  if (config.isProd) {
     const notifyClient = new NotifyClient(config.notifyApiKey)
     await notifyClient.sendEmail(config.notifyEmailTemplateKey, claim.email, {
       personalisation: { claimId: claim.claimId }
